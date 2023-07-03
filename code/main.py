dda17155cc882d89c9556fb11d94b9b324c30722
Hristina Adamovic <hristinacina@gmail.com>
2023-07-03 23:54:34 +0200

    restricted number of classes from 10 to 6

--- code/main.py
+++ code/main.py
@@ -2,7 +2,6 @@ import os
 
 import h5py
 import numpy as np
-from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
 # change file path to where you have stored your dataset
@@ -11,28 +10,27 @@ FILE_PATH = '../../data/Galaxy10_DECals.h5'
 
 def load_data():
     # To get the images and labels from file
+    print("loading dataset...")
     with h5py.File(FILE_PATH, 'r') as F:
         images = np.array(F['images'])
         print("loaded images")
         labels = np.array(F['ans'])
-        print("loaded ans")
-
-    # To convert the labels to categorical 10 classes
-    labels = keras.utils.to_categorical(labels, 10)
-
-    # To convert to desirable type
-    labels = labels.astype(np.float32)
-    images = images.astype(np.float32)
+        print("loaded labels")
+    images = images[:9983]  # cutting dataset to 6 from 10 classes
+    labels = labels[:9983]  # cutting dataset to 6 from 10 classes
 
     return images, labels
 
 
 def split_data():
     images, labels = load_data()
-    train_idx, test_idx = train_test_split(np.arange(labels.shape[0]), test_size=0.1)
-    return images[train_idx], labels[train_idx], images[test_idx], labels[test_idx]
+
+    print("splitting dataset to train and test datasets...")
+    x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.1)
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    print(x_train.shape, x_test.shape)
 
 
 if __name__ == '__main__':
-    print('Hello world!')
     split_data()
