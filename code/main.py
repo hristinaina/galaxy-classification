e0e1bcfea53fdc60fb2711c9fbd2f023a152c3a6
Hristina Adamovic <hristinacina@gmail.com>
2023-07-04 12:22:39 +0200

    added code for model testing

--- code/main.py
+++ code/main.py
@@ -6,6 +6,7 @@ from keras import Sequential
 from keras.callbacks import ReduceLROnPlateau
 from keras.optimizers import Adam
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+from keras.regularizers import l2
 from sklearn.model_selection import train_test_split
 
 # change file path to where you have stored your dataset
@@ -15,13 +16,19 @@ EPOCHS = 15
 
 
 def load_data():
-    # To get the images and labels from file
+    # get the images and labels from file
     print("loading dataset...")
     with h5py.File(FILE_PATH, 'r') as F:
         images = np.array(F['images'])
         print("loaded images")
         labels = np.array(F['ans'])
         print("loaded labels")
+
+    # shuffle data in dataset
+    indices = np.arange(len(images))
+    np.random.shuffle(indices)
+    images = images[indices]
+    labels = labels[indices]
 
     return images, labels
 
@@ -41,33 +48,35 @@ def split_data():
 
 def define_model():
     x_train, y_train, x_test, y_test = split_data()
-    model2 = Sequential()
+    model = Sequential()
 
-    model2.add(Conv2D(filters=6, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=(256, 256, 3)))
-    model2.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model2.add(Dropout(0.3))
+    model.add(Conv2D(filters=6, kernel_size=(5, 5), strides=(1, 1), kernel_regularizer=l2(0.002), activation='relu',
+                     input_shape=(256, 256, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.4))
 
-    model2.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
-    model2.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model2.add(Dropout(0.3))
+    model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(1, 1), kernel_regularizer=l2(0.003), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.4))
 
-    model2.add(Flatten())
-    model2.add(Dense(units=120, activation='relu'))
-    model2.add(Dense(units=84, activation='relu'))
-    model2.add(Dense(units=10, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(units=120, activation='relu', kernel_regularizer=l2(0.003)))
+    model.add(Dense(units=84, activation='relu', kernel_regularizer=l2(0.002)))
+    model.add(Dense(units=10, activation='softmax'))
 
     model_optimizer = Adam(lr=0.0005)
 
     reduceLR = ReduceLROnPlateau(monitor='accuracy', factor=.001, patience=1, min_delta=0.01, mode="auto")
 
-    model2.compile(optimizer=model_optimizer, loss='sparse_categorical_crossentropy', metrics=["accuracy"])
-    print("training stared...")
-    model2.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, callbacks=[reduceLR])
+    model.compile(optimizer=model_optimizer, loss='sparse_categorical_crossentropy', metrics=["accuracy"])
+    print("\nTRAINING STARTED...")
+    model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, callbacks=[reduceLR])
 
-    # print("testing started...")
-    # score = model2.evaluate(x_test, y_test, verbose=1)
-    # print('Test loss:', score[0])
-    # print('Test accuracy:', score[1])
+    gc.collect()
+    print("\nTESTING STARTED...")
+    test_loss, test_accuracy = model.evaluate(x_test, y_test)
+    print("Test Loss:", test_loss)
+    print("Test Accuracy:", test_accuracy)
 
 
 if __name__ == '__main__':
